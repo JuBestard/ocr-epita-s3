ec30d4ab23208a75312c01e7cbf2613c4338840b
hippolyte.pik <hippolyte.pik@epita.fr>
2022-10-11 03:46:33 +0200

    xor is push just call xor with i1 and i2 prototype: xor(int i1, int i2)

---
+++ xor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+const int w_and = 2;
+const int threshold_and = 3;
+
+const int w_or= 2;
+const int threshold_or = 1;
+
+int nand(int i1, int i2)
+{
+	return i1*w_and + i2*w_and >= threshold_and ? 0 : 1;
+}
+
+int or(int i1, int i2)
+{
+	return i1*w_or + i2*w_or >= threshold_or ? 1 : 0;	
+}
+
+int xor(int i1, int i2)
+{
+	return or(i1,i2)*w_and + nand(i2,i1) * w_and >= threshold_and ? 1 : 0; 
+}
---
+++ xor.h
@@ -0,0 +1,4 @@
+#pragma once
+int and(int i1, int i2);
+int nand(int i1, int i2);
+int or(int i1, int i2);
