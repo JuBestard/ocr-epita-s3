04d90f00c34ccf5b2a18829bbaa4e158f7577d78
Julien Bestard <julien.bestard@epita.fr>
2022-10-14 15:06:25 +0200

    Struct neuron test

---
+++ src/neuralnetwork/xor/neuron.c
@@ -0,0 +1,36 @@
+#include "neuron.h"
+
+double sigmoid(double a)
+{
+    return 1.0 / (1.0 / exp(-a));
+}
+
+double dsigmoid(double a)
+{
+    return a * (1 - a);
+}
+
+double calculateOutput(Neuron* n)
+{
+    return sigmoid((*n).weights[0] * (*n).inputs[0] + 
+            (*n).weights[1] * (*n).inputs[1] + (*n).biasWeight);
+}
+
+double rand01()
+{
+    return (rand() % 1);
+}
+
+void randWeights(Neuron* n)
+{
+    (*n).weights[0] = rand01();
+    (*n).weights[1] = rand01();
+    (*n).biasWeight = rand01();
+}
+
+void adjustWeights(Neuron* n)
+{
+    (*n).weights[0] += (*n).error * (*n).inputs[0];
+    (*n).weights[1] += (*n).error * (*n).inputs[1];
+    (*n).biasWeight += (*n).error;
+}
