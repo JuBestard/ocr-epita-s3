f0e3cf603d19eae5af8d18b51513ff96bb139c2b
Julien Bestard <julien.bestard@epita.fr>
2022-10-14 21:50:31 +0200

    xor done

--- src/neuralnetwork/xor/neuron.c
+++ src/neuralnetwork/xor/neuron.c
@@ -1,8 +1,10 @@
 #include "neuron.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 double sigmoid(double a)
 {
-    return 1.0 / (1.0 / exp(-a));
+    return 1.0 / (1.0 + exp(-a));
 }
 
 double dsigmoid(double a)
@@ -12,25 +14,25 @@ double dsigmoid(double a)
 
 double calculateOutput(Neuron* n)
 {
-    return sigmoid((*n).weights[0] * (*n).inputs[0] + 
-            (*n).weights[1] * (*n).inputs[1] + (*n).biasWeight);
+    return sigmoid(n->weights[0] * n->inputs[0] + 
+            n->weights[1] * n->inputs[1] + n->biasWeight);
 }
 
 double rand01()
 {
-    return (rand() % 1);
+    return ((double)rand() / (double)RAND_MAX);
 }
 
 void randWeights(Neuron* n)
 {
-    (*n).weights[0] = rand01();
-    (*n).weights[1] = rand01();
-    (*n).biasWeight = rand01();
+    n->weights[0] = rand01();
+    n->weights[1] = rand01();
+    n->biasWeight = rand01();
 }
 
 void adjustWeights(Neuron* n)
 {
-    (*n).weights[0] += (*n).error * (*n).inputs[0];
-    (*n).weights[1] += (*n).error * (*n).inputs[1];
-    (*n).biasWeight += (*n).error;
+    n->weights[0] += n->error * n->inputs[0];
+    n->weights[1] += n->error * n->inputs[1];
+    n->biasWeight += n->error;
 }
--- src/neuralnetwork/xor/xor.c
+++ src/neuralnetwork/xor/xor.c
@@ -1,44 +1,52 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "xor.h"
 #include "neuron.h"
 
-int train_inputs[4][2] = {{0, 0}, {1, 0}, {0, 1}, {1, 1}};
+double train_inputs[4][2] = {{0, 0}, {1, 0}, {0, 1}, {1, 1}};
 int train_outputs[4] = {0, 1, 1, 0};
 
 void train(int epoch) 
 {
-    Neuron* hidden1 = NULL;
-    Neuron* hidden2 = NULL;
-    Neuron* output = NULL;
+    
+    Neuron hidden1 = {{0.0, 0.0}, {0.0, 0.0}, 0, 0};
+    Neuron hidden2 = {{0.0, 0.0}, {0.0, 0.0}, 0, 0};
+    Neuron output = {{0.0, 0.0}, {0.0, 0.0}, 0, 0};
+
+    randWeights(&hidden1);
+    randWeights(&hidden2);
+    randWeights(&output);
 
     for (int j = 0; j < epoch; j++)
     {
+        printf("################## epoch %d ##################\n", j);
         for (int i = 0; i < 4; i++) 
         {
-            hidden1->inputs[0]=train_inputs[i][0];
-            hidden1->inputs[1]=train_inputs[i][0];
+            hidden1.inputs[0]=train_inputs[i][0];
+            hidden1.inputs[1]=train_inputs[i][1];
+            hidden2.inputs[0]=train_inputs[i][0];
+            hidden2.inputs[1]=train_inputs[i][1];
 
-            hidden2->inputs[0]=train_inputs[i][0];
-            hidden2->inputs[1]=train_inputs[i][0];
+            output.inputs[0] = calculateOutput(&hidden1);
+            output.inputs[1] = calculateOutput(&hidden2);
 
-            output->inputs[0] = calculateOutput(hidden1);
-            output->inputs[1] = calculateOutput(hidden2);
+            double result = calculateOutput(&output);
 
-            double result = calculateOutput(output);
-
-            printf("%f xor %f = %f", hidden1->inputs[0], hidden2->inputs[1],
+            printf("%f xor %f = %f\n", train_inputs[i][0], train_inputs[i][1],
                     result);
 
-            output->error = dsigmoid(result) * (train_outputs[i] - result);
-            adjustWeights(output);
+            output.error = dsigmoid(result) * (train_outputs[i] - result);
+            adjustWeights(&output);
 
-            hidden1->error = dsigmoid(calculateOutput(hidden1)) * output->error*output->weights[0];
-            hidden2->error = dsigmoid(calculateOutput(hidden2)) * output->error*output->weights[1];
+            hidden1.error = dsigmoid(calculateOutput(&hidden1)) 
+                * output.error * output.weights[0];
+            hidden2.error = dsigmoid(calculateOutput(&hidden2)) 
+                * output.error * output.weights[1];
 
-            adjustWeights(hidden1);
-            adjustWeights(hidden2);
-       }
+            adjustWeights(&hidden1);
+            adjustWeights(&hidden2);
+        }
+        printf("\n");
     }
 }
 
@@ -46,6 +54,5 @@ int main(int argc, char** argv)
 {
     if(argc != 2)
         return EXIT_FAILURE;
-
     train(atof(argv[1]));
 }
