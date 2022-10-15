9e5dba25ea4372a244df3fbda86117a564c5d5ea
Julien Bestard <julien.bestard@epita.fr>
2022-10-15 12:08:11 +0200

    solver func

--- src/app/solver/solver.c
+++ src/app/solver/solver.c
@@ -1,6 +1,6 @@
 #include "solver.h"
 
-int IsBoardValid(int grid[][9])
+int IsBoardValid(int board[][9])
 {
     for(int i = 1; i <= 9; i++)
     {
@@ -8,16 +8,49 @@ int IsBoardValid(int grid[][9])
         {
             int col = 0;
             int lig = 0;
-            int grid = 0;
             for(int k = 0; k < 9; k++)
             {
-                if(grid[0][k] == i)
+                if(board[j][k] == i)
                     lig += 1;
-                if(grid[k][k] == i)
+                if(board[k][j] == i)
                     col += 1;
                 if(col > 1 || lig > 1)
                     return 1;
             }
         }
+
+        for(int x = 0; x < 3; x++)
+        {
+            for(int y = 0; y < 3; y++)
+            {
+                int grid = 0;
+                for(int j = 0; j < 3; j++)
+                {
+                    for(int k = 0; k < 3; k++)
+                    {
+                        if(board[3 * x + j][3 * y + k] == i)
+                            grid += 1;
+                    }
+                }
+                if(grid > 1)
+                    return 0;
+            }
+        }
     }
+
+    return 1;
 }
+
+int IsSolved(int board[9][9])
+{
+    for(int i = 0; i < 9; i++)
+    {
+        for(int j = 0; j < 9; j++)
+        {
+            if(board[i][j] == 0)
+                return 0;
+        }
+    }
+    return 1;
+}
+
