8cdc161290053f1e273823c0053ce66238525ddf
Julien Bestard <julien.bestard@epita.fr>
2022-10-15 12:19:51 +0200

    solver done

--- src/app/solver/solver.h
+++ src/app/solver/solver.h
@@ -7,4 +7,11 @@ typedef struct Tuple
     int y = 0;
 } Tuple;
 
+int IsBoardValid(int board[9][9]);
+int IsSolved(int board[9][9]);
+Tuple FindEmpty(int board[9][9]);
+int Solve(int board[9][9]);
+
+void printGrid(int board[9][9]);
+
 #endif
