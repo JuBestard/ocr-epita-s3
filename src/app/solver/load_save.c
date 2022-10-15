60edf025e14a27cbc3b3682e8743f818ae32f1e1
Julien Bestard <julien.bestard@epita.fr>
2022-10-15 16:19:55 +0200

    loader saver grid done

--- src/app/solver/load_save.c
+++ src/app/solver/load_save.c
@@ -1,4 +1,5 @@
 #include "load_save.h"
+#include "solver.h"
 #include <err.h>
 #include <stdio.h>
 
@@ -19,7 +20,10 @@ void load_grid(char* path, int grid[9][9])
                 charActuel = fgetc(file);
                 if(charActuel != '\n' && charActuel != ' ')
                 {
-                    grid[i][j] = charActuel;
+                    if(charActuel == '.')
+                        grid[i][j] = 0;
+                    else
+                        grid[i][j] = charActuel - '0';
                     j++;
                 }
             }
@@ -30,5 +34,32 @@ void load_grid(char* path, int grid[9][9])
     else
     {
         errx(1, "Connot open file");
+    }
+}
+
+void save_grid(int grid[9][9], char* path)
+{
+    FILE* file = NULL;
+    file = fopen(path, "w");
+    
+    if(file != NULL)
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            if(i == 3 || i == 6)
+                fputc('\n',file);
+            for(int j = 0; j < 9; j++)
+            {
+                if(j == 3 || j == 6)
+                    fputc(' ',file);
+                fputc((char)(grid[i][j] + 48), file);
+            }
+            fputc('\n', file);
+        }
+        fclose(file);
+    }
+    else
+    {
+        errx(1, "Cannot open file");
     }
 }
--- src/app/solver/load_save.h
+++ src/app/solver/load_save.h
@@ -5,5 +5,5 @@
 #include <stdio.h>
 #include "err.h"
 void load_grid(char* file, int grid[9][9]);
-
+void save_grid(int grid[9][9], char* file);
 #endif
