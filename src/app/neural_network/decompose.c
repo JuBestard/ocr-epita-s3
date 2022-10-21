197deb107b755abb6cdcd7ddc5fc06801943f95e
hippolyte.pik <hippolyte.pik@epita.fr>
2022-10-21 14:43:43 +0200

    je push ca

--- src/app/neural_network/decompose.c
+++ src/app/neural_network/decompose.c
@@ -2,37 +2,40 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-int decompose(const char* file, float* rValue)
+void decompose(SDL_Surface* surface, float* rValue)
 {
-    SDL_Surface* temp = IMG_Load(file);
-    SDL_Surface* surface = SDL_ConvertSurfaceFormat(temp, SDL_PIXELFORMAT_RGB888, 0);
-    SDL_FreeSurface(temp);
-
     Uint8 r,g,b;
     Uint32* pixels = surface -> pixels;
     int w = surface -> w;
     int h = surface -> h;
     SDL_PixelFormat* format = surface->format;
     SDL_LockSurface(surface);
-    rValue = calloc(w*h, sizeof(float));
     for(int i=0; i<w*h;i++)
     {
         SDL_GetRGB(pixels[i], format, &r, &g, &b);
         float average = ((r+g+b)/3)/256;
         rValue[i] = average;
     }
-    return h;
 }
 
 int main(int argc, char** argv)
 {
     if (argc == 2)
         return EXIT_FAILURE;
-    float* tab = NULL;
-    int c = decompose(argv[1], tab);
-    for(int i=0; i<c*c;i++)
+
+    SDL_Surface* temp = IMG_Load(argv[1]);
+    if (temp ==NULL)
+	    errx(EXIT_FAILURE,"%s\n",SDL_GetError());
+    SDL_Surface* surface = SDL_ConvertSurfaceFormat(temp, SDL_PIXELFORMAT_RGB888, 0);
+    SDL_FreeSurface(temp);
+
+    int w = surface -> w;
+    int h = surface -> h;
+    float* tab = calloc(w*h, sizeof(float));
+    decompose(surface, tab);
+    for(int i=0; i<h*w;i++)
     {
-        if(i/c ==0 && i!=0)
+        if(i/w==0 && i!=0)
             printf("\n");
         printf("%f | ",tab[i]);
     }
