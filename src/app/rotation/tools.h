741a00b72f0795afba1dbc3567e6ab4c0b5d0088
Julien Bestard <julien.bestard@epita.fr>
2022-10-24 22:13:48 +0200

    rotation start

---
+++ src/app/rotation/tools.h
@@ -0,0 +1,22 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_video.h>
+
+SDL_Surface* load_image(const char* file);
+
+//Uint32 grayscale(Uint32 pixel_color, SDL_PixelFormat* format);
+
+//void apply_grayscale(SDL_Surface* surface, SDL_PixelFormat* format);
+
+//void contrast(SDL_Surface* surface, SDL_PixelFormat* format);
+
+//void invert(SDL_Surface* surface, SDL_PixelFormat* format, int status);
+
+void draw(SDL_Renderer* renderer, SDL_Texture* texture);
+
+#endif
