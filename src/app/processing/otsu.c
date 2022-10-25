72816acf3786b00e0dcc991a4677db74498ffc1d
Julien Bestard <julien.bestard@epita.fr>
2022-10-25 19:11:37 +0200

    otsu fix

--- src/app/processing/otsu.c
+++ src/app/processing/otsu.c
@@ -19,34 +19,40 @@ void init_hist(SDL_Surface* s, int width, int height, float* hist, int nbpixels)
         hist[i] /= nbpixels;
 }
 
-int otsu_threshold(float *hist, int nbpixels)
+Uint8 otsu_threshold(float *hist)
 {
-    float sumB = 0;
-    float wB = 0;
-    float max = 0.0;
-    float sum1 = 0;
-    int threshold = 0;
-    for(int i = 0; i < 256; i++)
+    float w0 = 0, w1 = 0, wT = 0;
+    float u0 = 0, u1 = 0, uT = 0;
+    float sum = 0, vk = 0, v_max = 0;
+    float threshold = 0;
+
+    for (size_t i = 0; i < 256; i++)
     {
-        sum1 += i * hist[i];
+        uT += i * hist[i];
+        wT += hist[i];
     }
-    for(int i = 0; i < 256; i++)
+
+    for (size_t i = 0; i < 256; i++)
     {
-        float wF = nbpixels - wB;
-        if (wB > 0 && wF > 0)
+        w0 += hist[i];
+        w1 = wT - w0;
+
+        sum += i * hist[i];
+        u0 = sum / w0;
+        u1 = (uT - sum) / w1;
+
+        // Maximizing inter-class variance
+        vk = w0 * w1 * (u0 - u1) * (u0 - u1);
+
+        // Find max vk = Find threshold
+        if (vk > v_max)
         {
-            float mF = (sum1 - sumB) / wF;
-            int val = wB * wF * ((sumB/wB) - mF) * ((sumB / wB) - mF);
-            if(val >= max)
-            {
-                threshold = i;
-                max = val;
-            }
+            threshold = i;
+            v_max = vk;
         }
-        wB = wB + hist[i];
-        sumB += i * hist[i];
     }
-    return threshold;
+
+    return (Uint8)threshold;
 }
 
 void otsu(SDL_Surface *image_surface)
@@ -57,7 +63,7 @@ void otsu(SDL_Surface *image_surface)
     float hist[256] = {0};
     init_hist(image_surface, width, height, hist, width*height);
 
-    Uint8 threshold = otsu_threshold(hist, width*height);
+    Uint8 threshold = otsu_threshold(hist);
     Uint8 r, g, b;
 
     for (size_t i = 0; i < width; i++)
