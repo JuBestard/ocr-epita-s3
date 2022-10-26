4161c5703a11fc58e75a6aa18796499166431076
Julien Bestard <julien.bestard@epita.fr>
2022-10-26 22:47:24 +0200

    lea solver adjust

--- src/app/solver/solver.c
+++ src/app/solver/solver.c
@@ -1,9 +1,11 @@
 #include "solver/solver.h"
 #include "solver/load_save.h"
 #include "err.h"
+#include "string.h"
 #include <stdio.h>
 
-int  RowCheck(int val, int board[N][N], int row)
+
+int  RowCheck(int val, int board[9][9], int row)
 // return 1 if the value is not already on the row
 {
     for (int col=0; col < 9; col++)
@@ -12,7 +14,7 @@ int  RowCheck(int val, int board[N][N], int row)
     return 1;
 }
 
-int ColCheck(int val, int board[N][N], int col)
+int ColCheck(int val, int board[9][9], int col)
 // return 1 if the value is not already on the columns
 {
     for (int row=0; row < 9; row++)
@@ -21,20 +23,20 @@ int ColCheck(int val, int board[N][N], int col)
     return 1;
 }
 
-int BoxCheck(int val, int board[N][N], int row, int col)
+int BoxCheck(int val, int board[9][9], int row, int col)
 // return 1 if the value is not already on the box (3x3)
 {
-    int i = row-(row%n), j = col-(col%n);  // ou encore : _i = 3*(i/3), _j = 3*(j/3);
-    for (row = i; row < i+n; row++)
-        for (col = j; col < j+n; col++)
+    int i = row-(row%3), j = col-(col%3);  // ou encore : _i = 3*(i/3), _j = 3*(j/3);
+    for (row = i; row < i+3; row++)
+        for (col = j; col < j+3; col++)
             if (board[row][col] == val)
                 return 0;
     return 1;
 }
 
-int Solve(int board[N][N], int position)
+int Solve(int board[9][9], int position)
 {
-    if (position == N*N)
+    if (position == 9*9)
         return 1;
 
     int row = position/9, col = position%9;
@@ -59,12 +61,14 @@ int Solve(int board[N][N], int position)
     return 0;
 }
 
-int main()
+int main(int argc, char** argv)
 {
+    if(argc != 2)
+        errx(1, "usage : ./solver <path-to-grid>");
+    char* result = strcat(argv[1], ".result");
     int grid[9][9];
-
-    load_grid("grid_00", grid);
-    Solve(grid);
-    save_grid(grid, "grid_00.result");
+    load_grid(argv[1], grid);
+    Solve(grid, 0);
+    save_grid(grid, result);
     return 0;
 }
