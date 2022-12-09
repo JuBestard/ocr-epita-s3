5858dd4f8de4c1b3e11893281cbc42a0a808a8f3
Benjamin DREYFUS <benjamin.dreyfus@epita.fr>
2022-12-09 07:35:39 +0100

    interface

--- src/ocr.c
+++ src/ocr.c
@@ -18,11 +18,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <gtk/gtk.h>
+
 #include "err.h"
 #include "math.h"
 
 #include <sys/types.h>
 #include <sys/stat.h>
+
+GtkBuilder *builder;
+GtkWindow *mainWindow;
+
+GtkFileChooser *fileChooser;
+GtkButton *buttonSolve;
+GtkButton *buttonClose;
+
+GFile *path;
+
+GtkImage *imageResolved;
+
+void on_close(GtkButton *buttonClose)
+{
+  gtk_window_close(mainWindow);
+}
+
+void on_solve(GtkButton *buttonSolve)
+{
+  if (path != NULL)
+    {
+      char *textpath = "given_grid/image_01.jpeg"; //g_file_get_relative_path(parent, path);
+      launch(textpath);
+    }
+
+  gtk_image_set_from_file(imageResolved, "out/resolve.bmp");
+}
+
+void on_choice(GtkFileChooser *fileChooser)
+{
+  path = gtk_file_chooser_get_current_folder_file(fileChooser);
+
+  char *textpath = g_file_get_path(path);
+  
+  gtk_image_set_from_file(imageResolved, textpath);
+
+  g_free(textpath);
+}
 
 void event_loop(SDL_Renderer* renderer, SDL_Texture* texture)
 {
@@ -130,7 +170,39 @@ int launch(char* path)
 
 int main(int argc, char** argv)
 {
-    if(argc != 2)
-        return usage();
-    return launch(argv[1]);
+    if(argc == 2)
+      return launch(argv[1]);
+    else
+      {
+	gtk_init(NULL, NULL);
+
+	builder = gtk_builder_new();
+	GError* error = NULL;
+	if (gtk_builder_add_from_file(builder, "window.glade", &error) == 0)
+	  {
+	    g_printerr("Error loading file: %s\n", error->message);
+	    g_clear_error(&error);
+	    return 1;
+	  }
+	
+	mainWindow = GTK_WINDOW(gtk_builder_get_object(builder, "mainWindow"));
+	
+	GtkButton* buttonTest = GTK_BUTTON(gtk_builder_get_object(builder, "buttonTest"));
+	fileChooser = GTK_FILE_CHOOSER(gtk_builder_get_object(builder, "fileChooser"));
+	buttonSolve = GTK_BUTTON(gtk_builder_get_object(builder, "buttonSolve"));
+	buttonClose = GTK_BUTTON(gtk_builder_get_object(builder, "buttonClose"));
+	
+	imageResolved = GTK_IMAGE(gtk_builder_get_object(builder, "imageResolved"));
+	path = NULL;
+	
+	g_signal_connect(mainWindow, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+        
+	g_signal_connect(fileChooser, "file-set", G_CALLBACK(on_choice), NULL);
+	g_signal_connect(buttonSolve, "clicked", G_CALLBACK(on_solve), NULL);
+	g_signal_connect(buttonClose, "clicked", G_CALLBACK(on_close), NULL);
+	
+	gtk_main();
+	
+	return 0;
+      }
 }
